622193fcf5e41210ba5bf176bf1057b43645dba6
Glidepixel <49463272+Glidepixel@users.noreply.github.com>
2022-01-13 16:32:40 -0500

    added the talons

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with
