573948a845dfeaa0756dc02bf02eefd829de057d
Rishi Desai <71404284+rdesai14@users.noreply.github.com>
2022-01-18 17:52:43 -0500

    Gampad test

--- src/main/java/frc/robot/Gamepad.java
+++ src/main/java/frc/robot/Gamepad.java
@@ -17,7 +17,46 @@ public class Gamepad {
     return -1 * controller.getRawAxis(0);
   }
 
+  public double getLJoystickY() {
+    return -1 * controller.getRawAxis(1);
+  }
+
+  public double getRJoystickX() {
+    return -1 * controller.getRawAxis(4);
+  }
+
+  public double getRJoystickY() {
+    return -1 * controller.getRawAxis(5);
+  }
+
+  public double getRTrigger() {
+    return controller.getRawAxis(3);
+  }
+  
+  public double getLTrigger() {
+    return controller.getRawAxis(2);
+  }
   public int getDPadAngle() {
     return controller.getPOV();
+  }
+
+  public boolean getB() {
+    return controller.getRawButton(2);
+  }
+
+  public boolean getX() {
+    return controller.getRawButton(3);
+  } 
+
+  public boolean getY() {
+    return controller.getRawButton(4);
+  } 
+  public boolean getLB() {
+    return controller.getRawButton(5);
+  }
+  
+  public boolean getRB() {
+    return controller.getRawButton(6);
+    
   }
 }
