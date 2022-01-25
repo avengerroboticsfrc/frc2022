3fdb7b975b62d86804eef1daeccbb0826fd1d246
Alliance Robotics <46729740+AvengerRobotics@users.noreply.github.com>
2022-01-25 18:35:45 -0500

    Created shooter class

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -6,8 +6,8 @@ import frc.robot.Constants;
 import frc.robot.subsystems.Limelight;
 public class Shooter {
     public final WPI_VictorSPX[] rightMotors = {
-        new WPI_VictorSPX(Constants.ShooterMotorPorts[5]),
-        new WPI_VictorSPX(Constants.ShooterMotorPorts[6])
+        new WPI_VictorSPX(Constants.ShooterMotorPorts[0]),
+        new WPI_VictorSPX(Constants.ShooterMotorPorts[1])
       };
 
 
