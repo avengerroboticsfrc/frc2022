e85fed64aa371978801460a84575627b4395be7b
Rishi Desai <71404284+rdesai14@users.noreply.github.com>
2022-01-27 17:45:19 -0500

    finish pneumatics class
    Co-authored-by: Karthik Hari <khari05@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/Pneumatics.java
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -2,9 +2,9 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 
 public class Pneumatics extends SubsystemBase {
   Compressor compressor = new Compressor(PneumaticsModuleType.CTREPCM);
@@ -14,7 +14,20 @@ public class Pneumatics extends SubsystemBase {
     compressor.enableDigital();
   }
 
+  public void start() {
+    compressor.enableDigital();
+  }
+
   public void stop() {
     compressor.disable();
   }
+
+  public void extend() {
+    intakeSolenoid.set(Value.kForward);
+  }
+
+  public void retract() {
+    intakeSolenoid.set(Value.kReverse);
+  } 
+
 }
