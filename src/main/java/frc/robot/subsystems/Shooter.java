7aeb953ff258f60254147ac7acfd72b972c9f66f
Rishi Desai <71404284+rdesai14@users.noreply.github.com>
2022-02-03 18:51:04 -0500

    Formatted Shooter Class and added comments

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,38 +1,50 @@
+//Imports
 package frc.robot.subsystems;
 
+
+//More Imports
 import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
+
+
+//Creates Shooter Class with its parent class as SubsystemBase
 public class Shooter extends SubsystemBase {
+  //Creates flyWheel motors
   private final WPI_VictorSPX[] flyWheelMotors = {
       new WPI_VictorSPX(Constants.MainRobot.shooterMotor.ports[0]),
       new WPI_VictorSPX(Constants.MainRobot.shooterMotor.ports[1])
   };
-
-
-
-
-  private final WPI_VictorSPX hoodMotor = new WPI_VictorSPX(Constants.MainRobot.hoodMotor.ports[0]);
-
-
-public void shooterPower(double speed){
-  flyWheelMotors[0].set(speed);
-}
-
-public void hoodPower(double speed){
-  hoodMotor.set(speed);
-}
-
-
-
+  //Shooter constructor
   Shooter() {
     super();
 
+    //flyWheelMotor1 will follow flyWheelMotor0
     flyWheelMotors[1].follow(flyWheelMotors[0]);
+    //Inverts flyWheelMotor1
     flyWheelMotors[1].setInverted(InvertType.OpposeMaster);
 
 
   }
+
+
+
+  //Creates Hood Motors
+  private final WPI_VictorSPX hoodMotor = new WPI_VictorSPX(Constants.MainRobot.hoodMotor.ports[0]);
+
+  //Method to make flyWheelMotors Move
+  public void shooterPower(double speed) {
+    flyWheelMotors[0].set(speed);
+  }
+
+  //Method to make hood move 
+  public void hoodPower(double speed) {
+    hoodMotor.set(speed);
+  }
+
+
+
+  
 }
