698a398a825d5853b3cdd83b04cafb0e690c5011
Rishi Desai <71404284+rdesai14@users.noreply.github.com>
2022-02-03 17:30:45 -0500

    Changing Pneumatics Hub to Rev

--- src/main/java/frc/robot/subsystems/Pneumatics.java
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -7,8 +7,8 @@ import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Pneumatics extends SubsystemBase {
-  Compressor compressor = new Compressor(PneumaticsModuleType.CTREPCM);
-  DoubleSolenoid intakeSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+  Compressor compressor = new Compressor(PneumaticsModuleType.REVPH);
+  DoubleSolenoid intakeSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 1);
 
   public Pneumatics() {
     compressor.enableDigital();
