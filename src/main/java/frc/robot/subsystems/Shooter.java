cf1cd3690e08aac95e1a8ceff3680e7196c5b5d7
Vineeth Pateel <95719853+Simgoo@users.noreply.github.com>
2022-02-03 17:40:00 -0500

    fixed indentation

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,7 +1,7 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
