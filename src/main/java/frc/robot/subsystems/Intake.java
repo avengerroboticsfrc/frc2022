b3a619184bdf1c54fd94e459c7286bd9c8fb9b79
Rishi Desai <71404284+rdesai14@users.noreply.github.com>
2022-02-03 19:15:53 -0500

    Comments to Intake Class

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,8 +2,11 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+//Imports
 package frc.robot.subsystems;
 
+
+//More Imports
 import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.Compressor;
@@ -13,28 +16,35 @@ import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
+
+//Creating Intake Class with SubsystemBase as an extension
 public class Intake extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
+
+  //Creating Compressor and Solenoid Classes
   Compressor compressor = new Compressor(PneumaticsModuleType.REVPH);
   DoubleSolenoid intakeSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 1);
 
 
 
-
+  //Creating Intake Motors
   private final WPI_VictorSPX[] intakeMotors = {
     new WPI_VictorSPX(Constants.MainRobot.intakeMotors.ports[12]),
     new WPI_VictorSPX(Constants.MainRobot.intakeMotors.ports[13])
-};
+  };
 
 
 
 
 
-
-public Intake() {
+  //Constructor
+  public Intake() {
+    //Super subsystemclass as well
     super();
 
+    //One intake motor will follow another
     intakeMotors[13].follow(intakeMotors[12]);
+    //Inverting one motor
     intakeMotors[13].setInverted(InvertType.OpposeMaster);
     compressor.enableDigital();
   }
@@ -43,25 +53,29 @@ public Intake() {
 
 
   
-
+  //Method Stoping Pneumatics System
   public void start() {
     compressor.enableDigital();
   }
 
+  //Method Stoping Pneumatics System
   public void stop() {
     compressor.disable();
   }
 
+  //Method Extending Intake with Solenoids & Pneumatic System
   public void extend() {
     intakeSolenoid.set(Value.kForward);
   }
 
+  //Method Retracting Intake with Solenoids & Pneumatic System
   public void retract() {
     intakeSolenoid.set(Value.kReverse);
   } 
 
+  //Setting Intake Power
   public void intakePower(double speed){
-      intakeMotors[12].set(speed);
+    intakeMotors[12].set(speed);
   }
 
 
