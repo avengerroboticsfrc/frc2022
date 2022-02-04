1e85b9a165743425961a83ab64c6fdf83e7d2a1b
Rishi Desai <71404284+rdesai14@users.noreply.github.com>
2022-02-03 20:10:51 -0500

    Comments to Lift Class

--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -4,30 +4,36 @@
 
 package frc.robot.subsystems;
 
+
+//Imports
 import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
+
+//Creates Lift Class
 public class Lift extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
 
+  //Creates Vertical Motors
   private final WPI_VictorSPX[] verticalMotors = {
       new WPI_VictorSPX(Constants.MainRobot.liftMotor.ports[0]),
       new WPI_VictorSPX(Constants.MainRobot.liftMotor.ports[1])
   };
 
+  //Creates Pitch Motors
   private final WPI_VictorSPX[] pitchMotors = {
     new WPI_VictorSPX(Constants.MainRobot.pitchMotor.ports[0]),
     new WPI_VictorSPX(Constants.MainRobot.pitchMotor.ports[1])
   };
 
-
+  //gives power to the vertical motors
   public void liftPower(double speed){
     verticalMotors[0].set(speed);
   }
 
+  //Gives power to pitch motors
   public void pitchPower(double speed){
     pitchMotors[0].set(speed);
   }
@@ -41,6 +47,7 @@ public class Lift extends SubsystemBase {
     // initialize the subsystem
     super();
 
+    //Pitch and vertical motors will follow their counterparts
     verticalMotors[1].follow(verticalMotors[0]);
     pitchMotors[1].follow(pitchMotors[0]);
     verticalMotors[1].setInverted(InvertType.OpposeMaster);
