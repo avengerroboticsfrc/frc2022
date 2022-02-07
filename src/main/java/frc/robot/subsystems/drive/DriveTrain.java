f57cd8f0f8f4afdd36b3980f680210f3e24065fb
Karthik Hari <35894769+khari05@users.noreply.github.com>
2022-02-07 17:29:10 -0500

    set up drivetrain for trajectory following

--- src/main/java/frc/robot/subsystems/drive/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/drive/DriveTrain.java
@@ -1,10 +1,16 @@
 package frc.robot.subsystems.drive;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public abstract class DriveTrain extends SubsystemBase {
@@ -13,6 +19,11 @@ public abstract class DriveTrain extends SubsystemBase {
 
   protected final DifferentialDrive driveTrain;
 
+  private final Gyro gyro = new ADXRS450_Gyro();
+
+  protected final DifferentialDriveOdometry odometry =
+      new DifferentialDriveOdometry(gyro.getRotation2d());
+
   /**
    * this method is called when the DriveTrainSubsystem class is initialized.
    */
@@ -20,27 +31,23 @@ public abstract class DriveTrain extends SubsystemBase {
     super();
 
     this.leftMotors = new WPI_TalonFX[] {
-      new WPI_TalonFX(left[0]),
-      new WPI_TalonFX(left[1])
+        new WPI_TalonFX(left[0]),
+        new WPI_TalonFX(left[1])
     };
     this.rightMotors = new WPI_TalonFX[] {
-      new WPI_TalonFX(right[0]),
-      new WPI_TalonFX(right[1])
+        new WPI_TalonFX(right[0]),
+        new WPI_TalonFX(right[1])
     };
-    // music = new Orchestra();
 
     driveTrain = new DifferentialDrive(
         leftMotors[0],
-        rightMotors[0]
-    );
+        rightMotors[0]);
 
-    // music.addInstrument((TalonFX) leftMotors[0]);
-    // music.addInstrument((TalonFX) leftMotors[1]);
-    // music.addInstrument((TalonFX) rightMotors[0]);
-    // music.addInstrument((TalonFX) rightMotors[1]);
-    // music.loadMusic("ssbb.chrp");
-    // System.out.println(music.play());
-    
+    leftMotors[0].configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
+    leftMotors[1].configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
+    rightMotors[0].configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
+    rightMotors[1].configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
+
     leftMotors[0].setNeutralMode(NeutralMode.Brake);
     leftMotors[1].setNeutralMode(NeutralMode.Brake);
     rightMotors[0].setNeutralMode(NeutralMode.Brake);
@@ -51,6 +58,51 @@ public abstract class DriveTrain extends SubsystemBase {
 
     rightMotors[0].setInverted(true);
     rightMotors[1].setInverted(InvertType.FollowMaster);
+
+    resetEncoders();
+  }
+
+  @Override
+  public void periodic() {
+    // Update the odometry in the periodic block
+    odometry.update(
+        gyro.getRotation2d(),
+        leftMotors[0].getSelectedSensorPosition(),
+        rightMotors[0].getSelectedSensorPosition() * -1
+    );
+  }
+
+  /**
+  * Returns the currently-estimated pose of the robot.
+
+  * @return The pose.
+  */
+  public Pose2d getPose() {
+    return odometry.getPoseMeters();
+  }
+
+
+  /**
+   * Returns the current wheel speeds of the robot.
+
+   * @return The current wheel speeds.
+   */
+  public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+    return new DifferentialDriveWheelSpeeds(
+        leftMotors[0].getSelectedSensorVelocity(),
+        rightMotors[0].getSelectedSensorVelocity() * -1
+      );
+  }
+
+
+  /**
+   * Resets the odometry to the specified pose.
+
+   * @param pose The pose to which to set the odometry.
+   */
+  public void resetOdometry(Pose2d pose) {
+    resetEncoders();
+    odometry.resetPosition(pose, gyro.getRotation2d());
   }
 
   /**
@@ -69,5 +121,27 @@ public abstract class DriveTrain extends SubsystemBase {
    */
   public void curvatureDrive(double speed, double rotation) {
     driveTrain.curvatureDrive(speed, rotation, false);
+  }
+
+  /** Resets the drive encoders to currently read a position of 0. */
+  public void resetEncoders() {
+    leftMotors[0].setSelectedSensorPosition(0);
+    leftMotors[1].setSelectedSensorPosition(0);
+    rightMotors[0].setSelectedSensorPosition(0);
+    rightMotors[0].setSelectedSensorPosition(0);
+  }
+
+  /**
+   * Gets the average distance of the two encoders.
+
+   * @return the average of the two encoder readings
+   */
+  public double getAverageEncoderDistance() {
+    return (
+        leftMotors[0].getSelectedSensorPosition()
+        + leftMotors[1].getSelectedSensorPosition()
+        + rightMotors[0].getSelectedSensorPosition() * -1
+        + rightMotors[1].getSelectedSensorPosition() * -1
+      ) / 4;
   }
 }
