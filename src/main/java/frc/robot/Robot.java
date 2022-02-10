f060d6488fcf51d9e01c7a270a58f6d3695a24b0
Alliance Robotics <46729740+AvengerRobotics@users.noreply.github.com>
2022-02-10 17:00:51 -0500

    burh

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -20,9 +20,7 @@ import frc.robot.subsystems.Tester;
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-
   private RobotContainer m_robotContainer;
-
   /**
    * This function is run when the robot is first started up and should be used
    * for any
@@ -119,4 +117,5 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
+
 }
