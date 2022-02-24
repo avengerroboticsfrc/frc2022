041e031b8f9cfdde366511ad8ee741ea3295c2bb
Karthik Hari <35894769+khari05@users.noreply.github.com>
2022-02-24 18:26:54 -0500

    add command to zero the encoder by reaching the reverse limit

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,17 +1,23 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 //Creates Shooter Class with its parent class as SubsystemBase
 public class Shooter extends SubsystemBase {
-  // Creates flyWheel motors
   private final WPI_VictorSPX[] flyWheelMotors = {
       new WPI_VictorSPX(Constants.MainRobot.shooterMotor.ports[0]),
       new WPI_VictorSPX(Constants.MainRobot.shooterMotor.ports[1])
   };
+  private final WPI_VictorSPX hoodMotor = new WPI_VictorSPX(Constants.MainRobot.hoodMotor.ports[0]);
+  private final WPI_TalonSRX turretTurner =
+      new WPI_TalonSRX(Constants.MainRobot.turretTurnMotor.ports[0]);
+
+  private final int TICKS_PER_ROTATION = 4096;
 
   // Shooter constructor
   public Shooter() {
@@ -21,18 +27,29 @@ public class Shooter extends SubsystemBase {
     flyWheelMotors[1].follow(flyWheelMotors[0]);
     // Inverts flyWheelMotor1
     flyWheelMotors[1].setInverted(InvertType.OpposeMaster);
+
+    turretTurner.configClearPositionOnLimitR(true, 50);
   }
 
-  // Creates Hood Motors
-  private final WPI_VictorSPX hoodMotor = new WPI_VictorSPX(Constants.MainRobot.hoodMotor.ports[0]);
-
   // Method to make flyWheelMotors Move
-  public void shooterPower(double speed) {
-    flyWheelMotors[0].set(speed);
+  public void shooterPower(double power) {
+    flyWheelMotors[0].set(power);
   }
 
   // Method to make hood move
-  public void hoodPower(double speed) {
-    hoodMotor.set(speed);
+  public void hoodPower(double power) {
+    hoodMotor.set(power);
+  }
+
+  public void turn(double power) {
+    turretTurner.set(power);
+  }
+
+  public void turnRotations(double ticks) {
+    turretTurner.set(ControlMode.Position, ticks * TICKS_PER_ROTATION);
+  }
+
+  public double getTurnPosition() {
+    return turretTurner.getSelectedSensorPosition(0);
   }
 }
